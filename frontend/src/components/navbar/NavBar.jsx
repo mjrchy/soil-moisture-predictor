7804f309a56b0b7a0ef22e723d4443b7594cd0df
Jiratchaya Thongsuthum <92805190+mjrchy@users.noreply.github.com>
2024-04-15 21:20:39 +0700

    Implement nav bar

--- frontend/src/components/navbar/NavBar.jsx
+++ frontend/src/components/navbar/NavBar.jsx
@@ -5,11 +5,35 @@ import { Link } from 'react-router-dom';
 function NavBar() {
   return (
     <div className='nav-vertical'>
-        <Link to="/">Predictor</Link>
-        <Link to="/currentvalues">Current Values</Link>
-        <Link to="/histogram">Histogram</Link>
-        <Link to="/scatterplot">Scatter Plot</Link>
-        <Link to="/heatmap">Heat Map</Link>
+        <div className='app-logo'>
+            <img src="/plant.png" />
+            <div className='app-name'>
+                <div className='name'>Soil Moisture</div>
+                <div className='name'>Predictor</div>
+            </div>
+        </div>
+        <div className='nav-link'>
+            <Link className='link' to="/">
+                <img src="/icons/predictor-icon.png" />
+                <div className="link-name">Predictor</div>
+            </Link>
+            <Link className='link' to="/currentvalues">
+                <img src="/icons/currentvalues-icon.png" />
+                <div className="link-name">Current Values</div>
+            </Link>
+            <Link className='link' to="/histogram">
+                <img src="/icons/histogram-icon.png" />
+                <div className="link-name">Histogram</div>
+            </Link>
+            <Link className='link' to="/scatterplot">
+                <img src="/icons/scatterplot-icon.png" />
+                <div className="link-name">Scatter Plot</div>
+            </Link>
+            <Link className='link' to="/heatmap">
+                <img src="/icons/heatmap-icon.png" />
+                <div className="link-name">Heat Map</div>
+            </Link>
+        </div>
     </div>
   )
 }
