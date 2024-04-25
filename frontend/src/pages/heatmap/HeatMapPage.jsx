689dc4106a8338f7062262bbf249295f192a53f0
Jiratchaya Thongsuthum <92805190+mjrchy@users.noreply.github.com>
2024-04-25 15:27:52 +0700

    Implement heatmap page and ImgContainer component

--- frontend/src/pages/heatmap/HeatMapPage.jsx
+++ frontend/src/pages/heatmap/HeatMapPage.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import './heatmap-page.css'
+import ImgContainer from '../../components/img_container/ImgContainer'
 
 function HeatMapPage() {
   return (
-    <div>HeatMapPage</div>
+    <div className="heatmap-page">
+      <div className="content">
+        <div className="topic">Heat Map</div>
+        <section>
+          <ImgContainer endpoint="visualize/heatmap" width="700px" />
+        </section>
+      </div>
+    </div>
   )
 }
 
