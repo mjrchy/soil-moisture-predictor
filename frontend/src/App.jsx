093ba23666576016d9df7a038b9c79b56469352f
Jiratchaya Thongsuthum <92805190+mjrchy@users.noreply.github.com>
2024-04-25 15:12:52 +0700

    Modify paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,9 @@ import NavBar from './components/navbar/NavBar';
 import PredictorPage from './pages/predictor/PredictorPage';
 import CurrentValuesPage from './pages/currentvalues/CurrentValuesPage';
 import HistogramPage from './pages/histogram/HistogramPage';
-import ScatterPlotPage from './pages/scatterplot/ScatterPlotPage';
 import HeatMapPage from './pages/heatmap/HeatMapPage';
+import DescriptivePage from './pages/descriptive/DescriptivePage';
+import LinePlotPage from './pages/line-plot/LinePlotPage';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -15,8 +16,9 @@ function App() {
       <Routes>
         <Route path='/' exact element={<PredictorPage/>}/>
         <Route path='/currentvalues' element={<CurrentValuesPage/>} />
-        <Route path='/histogram' element={<HistogramPage/>} />
-        <Route path='/scatterplot' element={<ScatterPlotPage/>} />
+        <Route path='/lineplot' element={<LinePlotPage/>} />
+        <Route path='/histograms' element={<HistogramPage/>} />
+        <Route path='/descriptive' element={<DescriptivePage/>} />
         <Route path='/heatmap' element={<HeatMapPage/>} />
       </Routes>
     </div>
