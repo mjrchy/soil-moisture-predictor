a0fbaad004879713d9e722a5d9eeef786fc508ea
Jiratchaya Thongsuthum <92805190+mjrchy@users.noreply.github.com>
2024-04-25 15:22:12 +0700

    Add the predictor output and customize it

--- frontend/src/pages/predictor/PredictorPage.jsx
+++ frontend/src/pages/predictor/PredictorPage.jsx
@@ -1,18 +1,54 @@
 import React from 'react'
 import './predictor-page.css'
 import InputPredictor from '../../components/input_predictor/InputPredictor'
+import axios from 'axios'; 
+import { useState } from 'react';
+import ValueBox from '../../components/value_box/ValueBox';
 
 function PredictorPage() {
+  const [formData, setFormData] = useState({
+    "air_humidity": 0.0,
+    "temperature": 0.0,
+    "pm2_5": 0.0,
+    "wind_speed": 0.0
+  });
+  
+  const [prediction, setPrediction] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+    };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      console.log(typeof(formData["air_humidity"]));
+      const response = await axios.post('http://127.0.0.1:8000/predict-soil-moisture', formData);
+      console.log(response);
+      setPrediction(response.data);
+      // console.log(response.data);
+      setError(null);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
   return (
     <div className='predictor-page'>
       <div className='content'>
         <div className='topic'>Soil Moisture Predictor</div>
         <div className='inputs'>
-          <InputPredictor src="/icons/airhumidity-icon.png" valuesName="Air Humidity" valuesUnit="g/m3" className="input-predictor" /> 
-          <InputPredictor src="/icons/pm25-icon.png" valuesName="Pm 2.5" valuesUnit="µg/m3" className="input-predictor" /> 
-          <InputPredictor src="/icons/temp-icon.png" valuesName="Temperature" valuesUnit="Celsius" className="input-predictor" /> 
+          <form onSubmit={handleSubmit}>
+            <InputPredictor src="/icons/airhumidity-icon.png" valuesName="Air Humidity" valuesUnit="g/m3" name="air_humidity" value={formData.air_humidity} className="input-predictor" change={handleChange} /> 
+            <InputPredictor src="/icons/pm25-icon.png" valuesName="Pm 2.5" valuesUnit="µg/m3" name="pm2_5" value={formData.pm2_5} className="input-predictor" change={handleChange} /> 
+            <InputPredictor src="/icons/temp-icon.png" valuesName="Temperature" valuesUnit="Celsius" name="temperature" value={formData.temperature} className="input-predictor" change={handleChange} /> 
+            <InputPredictor src="/icons/windspeed-icon.png" valuesName="Wind Speed" valuesUnit="km/h" name="wind_speed" value={formData.wind_speed} className="input-predictor" change={handleChange} /> 
+            <button type='submit'>Predict</button>
+          </form>
+          <ValueBox src="/icons/soilmoisture-icon.png" value={prediction} valueUnit="ohms (Ω)" valueName="Soil Moisture" className="value-box" />
         </div>
-        <button>Predict</button>
       </div>
     </div>
   )
