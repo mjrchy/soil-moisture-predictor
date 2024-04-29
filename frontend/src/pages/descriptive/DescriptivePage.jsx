146ba3fa7ee8d4dca73db4e73f37bd522f6effdb
Jiratchaya Thongsuthum <92805190+mjrchy@users.noreply.github.com>
2024-04-29 15:46:33 +0700

    Add descriptive box in descriptive page

--- frontend/src/pages/descriptive/DescriptivePage.jsx
+++ frontend/src/pages/descriptive/DescriptivePage.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import './descriptive-page.css';
+import DescriptiveBox from '../../components/descriptive_box/DescriptiveBox';
 
 function DescriptivePage() {
   return (
     <div className="descriptive-page">
-      
+      <div className="content">
+        <div className="topic">Descriptive</div>
+        <div className="descriptive-boxes">
+        <DescriptiveBox src='./icons/airhumidity-icon.png' valuesName="Air Humidity" mean="42" unit="mm" />
+        <DescriptiveBox src='./icons/airhumidity-icon.png' valuesName="Air Humidity" mean="42" unit="mm" />
+        <DescriptiveBox src='./icons/airhumidity-icon.png' valuesName="Air Humidity" mean="42" unit="mm" />
+        <DescriptiveBox src='./icons/airhumidity-icon.png' valuesName="Air Humidity" mean="42" unit="mm" />
+        <DescriptiveBox src='./icons/airhumidity-icon.png' valuesName="Air Humidity" mean="42" unit="mm" />
+        </div>
+      </div>
     </div>
   )
 }
