bfb85eaf8a97f47b22cf187c9b456c74f1cbad93
Jiratchaya Thongsuthum <92805190+mjrchy@users.noreply.github.com>
2024-05-08 12:59:15 +0700

    Request statistics api

--- frontend/src/pages/descriptive/DescriptivePage.jsx
+++ frontend/src/pages/descriptive/DescriptivePage.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
 import './descriptive-page.css';
 import DescriptiveBox from '../../components/descriptive_box/DescriptiveBox';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import axios from 'axios'; 
+
 
 function DescriptivePage() {
+  const [statistics, setStatistics] = useState('');
+
+  useEffect(() => {
+    const fetchStatistics = async () => {
+      try {
+        const response = await fetch(`http://127.0.0.1:8000/statistics`);
+        if (response.ok) {
+          const blob = await response.blob();
+          setStatistics(URL.createObjectURL(blob));
+          console.log(response);
+        } else {
+          console.error('Failed to fetch statistics:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error fetching statistics:', error);
+      }
+    };
+
+    fetchStatistics();
+
+    return () => {
+      URL.revokeObjectURL(statistics);
+    };
+  }, []);
+
   return (
     <div className="descriptive-page">
       <div className="content">
